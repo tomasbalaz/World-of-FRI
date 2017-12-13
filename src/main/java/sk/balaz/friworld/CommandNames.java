85a0ca7cb591460d2bea976b9519bd8487ebc3c2
Tomas Balaz <tomas.balaz@asseco-ce.com>
2017-12-13 15:36:22 +0100

    Create CommandNames class

--- src/main/java/sk/balaz/friworld/CommandNames.java
+++ src/main/java/sk/balaz/friworld/CommandNames.java
@@ -2,14 +2,46 @@ package sk.balaz.friworld;
 
 import java.util.List;
 
+/**
+ * Contains all valid commands which are used in Game 
+ * and check if entered command is valid.
+ * 
+ * @author tomas.balaz
+ *
+ */
 public class CommandNames {
 	
+	/* All valid command names */
     private List<String> commandNames;
     
-    //definÌcia vöetk˝ch platn˝ch prÌkazov
+    /**
+     * Initialize all valid commands which are used in Game.
+     */
+    public CommandNames() {
+    	initializeCommandNames();
+	}
     
-    //n·zvy prÌkazov ñ pole reùazcov
+    /**
+     * Checks if entered command is valid in game.
+     * 
+     * @param command Command 
+     * @return True if entered command is valid, otherwise false.
+     */
+    public boolean isValidCommand(String command) {
+    	if(command != null) {
+    		for (String commandName : commandNames) {
+    			if(command.equals(commandName)) {
+    				return true;
+    			}
+    		}	
+    	}
+    	return false;
+    }
     
-    //overuje platnosù prÌkazu
+    /* Initialize all valid commands which are used in Game */
+    private void initializeCommandNames() {
+    	commandNames.add("UP");
+    	commandNames.add("DOWN");
+    }
 
 }
