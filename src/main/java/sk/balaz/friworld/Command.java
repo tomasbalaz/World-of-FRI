7c2a82afd79a9b85bd23c4ad6b09255fba4eeb19
Tomas Balaz <tomas.balaz@asseco-ce.com>
2017-12-14 13:18:50 +0100

    Add Command class

--- src/main/java/sk/balaz/friworld/Command.java
+++ src/main/java/sk/balaz/friworld/Command.java
@@ -1,13 +1,55 @@
 package sk.balaz.friworld;
 
+/**
+ * Represents user commands used in the game.
+ * For example "USE", "UP", "DOWN", "EXIT"
+ * 
+ * @author tomas.balaz
+ *
+ */
 public class Command {
 	
-	//inštancie sú nemenite¾né objekty
-    
-	//reprezentuje zadanı príkaz
-	    
-	//príkaz má 2 èasti: názov a parameter
-	    
-	//overuje platnos príkazu (nazov != null)
-
+	/* Command name */
+	private String name;
+	
+	/* Command parameter */
+	private String parameter;
+	
+	/**
+	 * Creates command with name and parameter.
+	 * 
+	 * @param name - Command name
+	 * @param parameter - Command parameter
+	 */
+	public Command(String name, String parameter) {
+		this.name = name;
+		this.parameter =  parameter;
+	}
+	
+	/**
+	 * Returns Command name.
+	 * 
+	 * @return Command name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns Command parameter.
+	 * 
+	 * @return Command parameter
+	 */
+	public String getParameter() {
+		return parameter;
+	}
+	
+	/**
+	 * Checks if command has stored parameter.
+	 * 
+	 * @return True if command has stored parameter, otherwise false.
+	 */
+	public boolean hasParameter() {
+		return parameter != null;
+	}
 }
