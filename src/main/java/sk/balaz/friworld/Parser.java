709fce6f27cb4bf48676e92fc3307da00209820b
Tomas Balaz <tomas.balaz@asseco-ce.com>
2017-12-19 13:59:59 +0100

    Add parse method into Parser class

--- src/main/java/sk/balaz/friworld/Parser.java
+++ src/main/java/sk/balaz/friworld/Parser.java
@@ -1,13 +1,43 @@
 package sk.balaz.friworld;
 
+/**
+ * Parses user entered command with attributes and check if command with attributes is defined
+ * and creates class {@link Command} .
+ * 
+ * @author tomas.balaz
+ *
+ */
 public class Parser {
 	
-    //èíta vstup hráèa z terminálu
-
-    //rozpoznáva vo vstupe slová príkazov
-
-    //príkazy kontroluje prostredníctvom inštancie triedy NazvyPríkazov
-
-    //vytvára inštancie triedy Prikaz
+	/* Command index in user entered command String */
+	private static final int COMMAND_INDEX = 0;
+	
+	/* Parameter index in user entered command String */
+	private static final int PARAMETER_INDEX = 1;
+	
+	/* String delimits user entered Command and Command parameters  */
+	private static final String COMMAND_SPLITTER = " ";
+	
+	/* Holds all valid game commands */
+	private final CommandNames commandNames = new CommandNames();
+	
+	/**
+	 * Parses user entered command to Command and Command parameters.
+	 * If command is defined then return {@link Command} class.
+	 * If command is undefined then return null.
+	 *  
+	 * @param command user entered command String
+	 * @return @link Command} class.
+	 */
+	public Command parse(String command) {
+		String[] commandAttributes = command.split(COMMAND_SPLITTER);
+		String commandName = commandAttributes[COMMAND_INDEX];
+		String commandParameter = commandAttributes[PARAMETER_INDEX];
+		if(!commandNames.isValidCommand(commandName)) {
+			System.err.println("Neznamy command");
+			return null;
+		}
+		return new Command(commandName, commandParameter);
+	}
 
 }
